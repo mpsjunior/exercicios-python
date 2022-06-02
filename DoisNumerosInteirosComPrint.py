56cda21e7cc7eb95b486cdcc9fd4003ffb7efb0e
mpsjunior <mpsjunior2017@gmail.com>
2022-06-01 23:04:10 -0300

    dois números interios printando na tela se é + - ou =

---
+++ DoisNumerosInteirosComPrint.py
@@ -0,0 +1,9 @@
+n1 = int(input("Primeiro Número: "))
+n2 = int(input("Segundo Número: "))
+
+if n1>n2:
+    print("O Primeiro valor é maior!")
+elif n2>n1:
+    print("O segundo número é maior!") 
+else:
+    print("Os dois valores são iguais")   
