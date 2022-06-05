73269e43de5c4dcd3302e759043509d348533ba7
mpsjunior <mpsjunior2017@gmail.com>
2022-06-05 15:50:32 -0300

    Exercício de Classe em Python

---
+++ CalcularCuboCriandoClass.py
@@ -0,0 +1,11 @@
+class cubo:
+    def __init__(self, valor):
+        self.x = valor
+        print("Objeto criado!")
+    def calcula_cubo(self):
+        cubo = self.x * self.x * self.x    
+        return "cubo calculado: " + str(cubo)
+
+teste = cubo(6)
+c = teste.calcula_cubo()
+print(c)
