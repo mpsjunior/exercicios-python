3f32cfafcef9b7facf69d6eaedef7b741ba1e186
mpsjunior <mpsjunior2017@gmail.com>
2022-06-07 18:32:50 -0300

    feito as modificações, teste ok

--- FibonacciComFor.py
+++ FibonacciComFor.py
@@ -1,22 +1,15 @@
-from ast import Str
-
-
 n = int(input("Digite a quantidade de sequências: "))
 cont1 = 0
 cont2 = 1
-i = 1
-  
 if (n == 1):
    print(cont1)
    exit()
-print("{} {}" .format(cont1, cont2), end=" ")
+print("{} {}".format(cont1, cont2), end= " ")
 
-while (i <= n - 2):
+for i in range(n-2):
     cont3 = cont1 + cont2
+    cont1 = cont2
+    cont2 = cont3 
     print("{}".format(cont3), end=" ")
-    cont1 = cont2 
-    cont2 = cont3
-    i += 1
-   
-print("fim")
     
+print(" fim")
