8d344ca58969ed308814684d8763edefec64ef3b
mpsjunior <mpsjunior2017@gmail.com>
2022-06-16 22:05:43 -0300

    mais um exercicio basico de dicionario com o uso de vetores

---
+++ exercicioBasicoDeDicionario.py
@@ -0,0 +1,10 @@
+prof = {
+    "nome": "Jonathan",
+    "pais": {"mae": "Andreia" , "pai": "Jailson"}
+}
+
+primeiraRodada = prof["nome"]
+print(primeiraRodada)
+
+segundaRodada = prof["pais"]
+print(segundaRodada)
