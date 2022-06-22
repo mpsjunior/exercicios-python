899906a74a828f88e6c95a5482b0767b57d50cbf
mpsjunior <mpsjunior2017@gmail.com>
2022-06-22 10:29:10 -0300

    Exemplos simples de vetores

---
+++ formaSimplificadaDosVetores.py
@@ -0,0 +1,5 @@
+text = "Hello world"
+print(text[0])
+
+textlist = list(text)
+print(textlist)
---
+++ formaSimplificadaDosVetores2.py
@@ -0,0 +1,6 @@
+vetor =["a", "b","c"]
+print(vetor)
+vetor.append("d")
+print(vetor)
+vetor.pop(0)
+print(vetor)
